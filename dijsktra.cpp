a0ccc60f5c0e6cb47807f90b3d44ce08b7589f8a
cse2014 <friends.itsabhihere@gmail.com>
2016-10-15 07:06:16 +0530

    Update dijsktra.cpp

--- dijsktra.cpp
+++ dijsktra.cpp
@@ -8,38 +8,31 @@ int minDistance(int dist[], bool sptSet[])
    for (int v = 0; v < V; v++)
      if (sptSet[v] == false && dist[v] <= min)
          min = dist[v], min_index = v;
- 
    return min_index;
 }
- 
 int printSolution(int dist[], int n)
 {
    printf("Vertex   Distance from Source\n");
    for (int i = 0; i < V; i++)
       printf("%d \t\t %d\n", i, dist[i]);
 }
- 
 void dijkstra(int graph[][20], int src)
 {
      int dist[V];     
      bool sptSet[V];  
      for (int i = 0; i < V; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
- 
      dist[src] = 0;
- 
      for (int count = 0; count < V-1; count++)
      {
        int u = minDistance(dist, sptSet);
        sptSet[u] = true;
        for (int v = 0; v < V; v++)
-          if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-                                       && dist[u]+graph[u][v] < dist[v])
+          if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u]+graph[u][v] < dist[v])
             dist[v] = dist[u] + graph[u][v];
      }
      printSolution(dist, V);
 }
- 
 int main()
 {
  	printf("enter the no of nodes ");
@@ -51,7 +44,6 @@ int main()
 	dijkstra(graph, 0);
      return 0;
 } 
-
 //nodes-4
 //1 0 5 3
 //2 0 0 1
